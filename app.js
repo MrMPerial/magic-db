6f4195a048ab736d6b3a0e01ddfabb021a85eca6
MrMPerial <christopher.mitchell26@gmail.com>
2018-12-31 10:17:38 -0600

    Fixed some structure issues and finished setting up the App file.

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require('express');
- 
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const mongodb = require('./utils/mongodb.utils');
+const routes = require('./routes/index');
+
+// Initialize App
 const app = express();
- 
+
+const cards = require('./routes/cards');
+
+// View Engine EJS
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
- 
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongodb.createEventListeners();
+mongodb.connect();
+
 app.get('/', (req, res) => {
   res.render('main');
 });
- 
-// app.use('/endpoint', file);
- 
+
+app.use('/cards', cards);
+
 module.exports = app;
