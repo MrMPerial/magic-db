f27005939f72d47de2bbf29502ae64721ec8c9bd
MrMPerial <christopher.mitchell26@gmail.com>
2019-01-01 11:10:24 -0600

    Added a new way to collect and store the data from the API ( from the UI )

--- models/card.model.js
+++ models/card.model.js
@@ -9,7 +9,18 @@ let Card = module.exports = mongoose.model('Card', CardSchema);
 
 module.exports.addToDB = (title) => {
   let newCard = new Card({
-    title: title
+    title: title,
+    manaCost: manaCost,
+    colorIdentity: colorIdentity,
+    type: type,
+    rarity: rarity,
+    setName: setName,
+    artist: artist,
+    number: number,
+    power: power,
+    toughness: toughness,
+    multiverseid: multiverseid,
+    imageUrl: imageUrl
   });
 
   return newCard.save();
--- routes/cards/index.js
+++ routes/cards/index.js
@@ -21,12 +21,36 @@ router.get('/findCard', (req, res) => {
 
 // POST REQUESTS
 router.post('/addNewCard', (req, res) => {
-  let title = req.body.title;
+  let cardName = req.body.cardName;
   let image = req.body.imageUrl;
+  let manaCost = req.body.manaCost;
+  let colorIdentity = req.body.colorIdentity;
+  let type = req.body.type;
+  let rarity = req.body.rarity;
+  let setName = req.body.setName;
+  let artist = req.body.artist;
+  let number = req.body.number;
+  let power = req.body.power;
+  let toughness = req.body.toughness;
+  let multiverseid = req.body.multiverseid;
+  
 
-  Card.addToDB(title);
+  console.log(cardName);
+  console.log(image);
+  console.log(manaCost);
+  console.log(colorIdentity);
+  console.log(type);
+  console.log(rarity);
+  console.log(setName);
+  console.log(artist);
+  console.log(number);
+  console.log(power);
+  console.log(toughness);
+  console.log(multiverseid);
 
-  res.status(200).send('Your card has been added to the system.');
+  // Card.addToDB(title);
+
+  res.status(200).end('Your card has been added to the system.');
 });
 
 module.exports = router;
