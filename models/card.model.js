bcfc97cccd2a0d51890aeff9e3f0fc3d2ffe60ec
MrMPerial <christopher.mitchell26@gmail.com>
2019-01-02 18:38:10 -0600

    Corrected the DB Schema to include selected content.

--- models/card.model.js
+++ models/card.model.js
@@ -2,25 +2,37 @@ const mongoose = require('mongoose');
 const mtg = require('mtgsdk');
 
 const CardSchema = mongoose.Schema({
-  title: String
+  cardName: String,
+  manaCost: String,
+  colorIdentity: Array,
+  type: String,
+  rarity: String,
+  setName: String,
+  artist: String,
+  number: String,
+  power: String,
+  toughness: String,
+  multiverseid: Number,
+  imageUrl: String
 });
 
 let Card = module.exports = mongoose.model('Card', CardSchema);
 
-module.exports.addToDB = (title) => {
+module.exports.addToDB = (package) => {
+
   let newCard = new Card({
-    title: title,
-    manaCost: manaCost,
-    colorIdentity: colorIdentity,
-    type: type,
-    rarity: rarity,
-    setName: setName,
-    artist: artist,
-    number: number,
-    power: power,
-    toughness: toughness,
-    multiverseid: multiverseid,
-    imageUrl: imageUrl
+    cardName: package.cardName,
+    manaCost: package.manaCost,
+    colorIdentity: package.colorIdentity,
+    type: package.type,
+    rarity: package.rarity,
+    setName: package.setName,
+    artist: package.artist,
+    number: package.number,
+    power: package.power,
+    toughness: package.toughness,
+    multiverseid: package.multiverseid,
+    imageUrl: package.imageUrl
   });
 
   return newCard.save();
--- routes/cards/index.js
+++ routes/cards/index.js
@@ -21,34 +21,23 @@ router.get('/findCard', (req, res) => {
 
 // POST REQUESTS
 router.post('/addNewCard', (req, res) => {
-  let cardName = req.body.cardName;
-  let image = req.body.imageUrl;
-  let manaCost = req.body.manaCost;
-  let colorIdentity = req.body.colorIdentity;
-  let type = req.body.type;
-  let rarity = req.body.rarity;
-  let setName = req.body.setName;
-  let artist = req.body.artist;
-  let number = req.body.number;
-  let power = req.body.power;
-  let toughness = req.body.toughness;
-  let multiverseid = req.body.multiverseid;
-  
 
-  console.log(cardName);
-  console.log(image);
-  console.log(manaCost);
-  console.log(colorIdentity);
-  console.log(type);
-  console.log(rarity);
-  console.log(setName);
-  console.log(artist);
-  console.log(number);
-  console.log(power);
-  console.log(toughness);
-  console.log(multiverseid);
+  let package = {
+    cardName: req.body.cardName,
+    image: req.body.imageUrl,
+    manaCost: req.body.manaCost,
+    colorIdentity: req.body.colorIdentity,
+    type: req.body.type,
+    rarity: req.body.rarity,
+    setName: req.body.setName,
+    artist: req.body.artist,
+    number: req.body.number,
+    power: req.body.power,
+    toughness: req.body.toughness,
+    multiverseid: req.body.multiverseid
+  };
 
-  // Card.addToDB(title);
+  Card.addToDB(package);
 
   res.status(200).end('Your card has been added to the system.');
 });
